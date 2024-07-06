3e46e1404507da1849b392f4987e2a85797c6c59
moheros <qxin829@gmail.com>
2024-07-06 23:05:01 +0900

    ポートを明示的に指定

--- lineResServer.py
+++ lineResServer.py
@@ -55,7 +55,8 @@ def handle_message(event):
     LINE_ACCESS_TOKEN.multicast(user_id, TextSendMessage("成功！"))
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 

60e5f728e819894cfddcd09972bffe03068b8ebc
moheros <qxin829@gmail.com>
2024-08-03 16:18:10 +0900

    herokuのログ確認が可能なためスプレッドシート書き込みはなし

--- lineResServer.py
+++ lineResServer.py
@@ -2,9 +2,6 @@
 import os
 import random
 import string
-# import gspread
-# import json
-# from google.oauth2 import service_account
 from argparse import ArgumentParser
 from dotenv import load_dotenv
 from flask import Flask, request, abort
@@ -64,25 +61,6 @@ def handle_message(event):
     connection = dbConnect.db_connect()
     cursor = connection.cursor()
     cursor.execute("SELECT user_id FROM users WHERE user_id = %s", (userId,))
-
-
-
-    # # 環境変数から認証情報を取得
-    # credentials_info = json.loads(os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON'))
-    # # Google Sheets APIにアクセスするためのクレデンシャルを作成
-    # credentials = service_account.Credentials.from_service_account_info(
-    #     credentials_info,
-    #     scopes=["https://www.googleapis.com/auth/spreadsheets"]
-    # )
-    # # gspreadでGoogle Sheetsにアクセス
-    # gc = gspread.authorize(credentials)
-    # # スプレッドシートを開く
-    # sheet_name = 'DailySmartLog'
-    # wks = gc.open(sheet_name).sheet1
-    # # セルにデータを更新
-    # wks.update_cell(1, 1, 'Pythonから入力')
-
-
 
     # アクセスユーザーが初回登録か判定しワンタイムキーの発行とDBへの追加
     registCheckFlag = cursor.fetchone()

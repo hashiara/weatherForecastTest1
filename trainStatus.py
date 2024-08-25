5051a82df2bff8a7401798a16535ebe3126d008e
moheros <qxin829@gmail.com>
2024-08-25 14:39:57 +0900

    路線情報に通知の条件追加

--- trainStatus.py
+++ trainStatus.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from urllib.request import urlopen, Request
 import json
-from datetime import datetime
+from datetime import datetime, time
 from linebot.exceptions import LineBotApiError
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
@@ -32,15 +32,19 @@ def main():
     # DB接続
     connection = dbConnect.db_connect()
     cursor = connection.cursor()
-    cursor.execute("SELECT users.user_id, train.area_code, train.rail_order FROM users JOIN train ON users.user_id = train.user_id")
+    cursor.execute("""SELECT users.user_id, train.area_code, train.rail_order, train.last_train_status 
+                   FROM users 
+                   JOIN train ON users.user_id = train.user_id""")
 
-    for row in cursor:
+    rows = cursor.fetchall()
+    for row in rows:
         user_id = row[0]
         area_code = str(row[1])
         rail_order = row[2]
+        last_train_status = row[3]
         if all(val is not None for val in [user_id, area_code, rail_order]):
 
-            # 路線情報メッセージ送信
+            # 路線情報url取得
             url_trainStatus = "https://ntool.online/data/train_all.json"
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
@@ -57,16 +61,46 @@ def main():
             data = res_json.get('data', {})
             src = res_json.get('src', {})
 
+            # 取得した最新更新時刻のフォーマット変換
             timestamp_s = last_updated / 1000
             dt = datetime.fromtimestamp(timestamp_s)
             formatted_last_updated = dt.strftime('%Y年%m月%d日 %H時%M分')
+
+            # 時刻比較用
+            current_time = datetime.now().time()
+            start_time = time(8, 0, 0)
+            end_time = time(23, 59, 59)
             
+            # 路線情報メッセージ送信
             for area in data:
                 if area == area_code:
                     rails = data[area]
                     rail = rails[rail_order]
                     if len(rails) > 0 and rail:
-                        send_to_line(line_access_token, user_id, rail, formatted_last_updated, src)
+                        # 現在時刻が0時を過ぎていない
+                        # rail['status']がlast_train_statusと異なる
+                        # last_train_statusがNone
+                        if (start_time <= current_time <= end_time) and (rail['status'] != last_train_status or last_train_status is None):
+                            if rail['status'] == last_train_status:
+                                continue
+                            send_to_line(line_access_token, user_id, rail, formatted_last_updated, src)
+                            try:
+                                cursor.execute("""UPDATE train 
+                                               SET last_train_status = %s
+                                               WHERE user_id = %s""", 
+                                               (rail['status'], user_id))
+                                connection.commit()
+                            except Exception as e:
+                                print(f"Error updating {user_id}: {e}")
+                        else:
+                            try:
+                                cursor.execute("""UPDATE train 
+                                               SET last_train_status = Null
+                                               WHERE user_id = %s""", 
+                                               (user_id,))
+                                connection.commit()
+                            except Exception as e:
+                                print(f"Error updating {user_id}: {e}")
 
     # 接続を閉じる
     cursor.close()
